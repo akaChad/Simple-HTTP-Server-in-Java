5153c683c4a6f43f6e281c1ac53cccf2a2937d96
Shaun Hallman <hallmanshaun6@gmail.com>
2026-08-24 15:48:21 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+        // spin forever
+        while (true){
+            Socket clientSocket = server.accept();
+            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            while(!line.isEmpty()) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+
+        }
+
+
+
+    }
+}
